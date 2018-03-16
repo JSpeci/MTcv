858e4d9d8963103f718115089c0e87dda22c4af0
Jan_Specian <jan.specian@tul.cz>
2018-03-16 11:12:21 +0100

    prvni cast dokoncena, refaktorovana, funkcni k odevzdani

--- cv04/cv04.py
+++ cv04/cv04.py
@@ -4,50 +4,69 @@ import matplotlib.pyplot as plt
 import struct
 
 
-im1 = cv2.imread('Cv04_porucha2.bmp')
-et1 = cv2.imread('Cv04_porucha2_etalon.bmp')
+def jasova_korekce(path_img, path_et, c = 255):
+    #nacteni
+    im1 = cv2.imread(path_img)
+    et1 = cv2.imread(path_et)
 
-im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
-et1 = cv2.cvtColor(et1, cv2.COLOR_BGR2RGB)
+    #prevod z BGR do RGB
+    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
+    et1 = cv2.cvtColor(et1, cv2.COLOR_BGR2RGB)
 
-res = np.zeros(np.shape(et1)).astype('double')
+    #tvorba vysledne matice o stejnych rozmerech jako vstup
+    res = np.zeros(np.shape(et1)).astype('double')
 
+    # in double
+    im = im1.astype('double')
+    # interval (0,1)
+    im = np.multiply(im,(1.0/255))
 
-# in double
-im = im1.astype('double')
-im = np.multiply(im,(1.0/255))
-et = et1.astype('double')
-et = np.multiply(et,(1.0/255))
-et = np.multiply(1.0/et, 1.0)
+    # tam kde je etalon nula - dojde k deleni nulou, upravim z nul na jednicky
+    et1[et1 == 0] = 1 # pozdeji to bude 1/255 misto 0/255
 
-for i in range(0,3):
-    #print(np.shape(res[:,:,i]))
-    res[:,:,i] = np.multiply(et[:,:,i],im[:,:,i])
+    # etalon in double
+    et = et1.astype('double')
+    # etalon v intervalu (0,1)
 
-c = 255.0
-res = np.multiply(res, c)
-res = res.astype('uint8')
+    et = np.multiply(et,(1.0/255))
+    # prevracena hodnota etalonu po prvcich
+    et = np.multiply(1.0/et, 1.0)
 
+    #nasobeni matic po prvcich a po vstvach -i
+    for i in range(0,3):
+        res[:,:,i] = np.multiply(et[:,:,i],im[:,:,i])
 
-plt.figure()
-plt.subplot(2,3,1)
-plt.imshow(et1)
+    # prevod zpet do urovni (0,255)
+    res = np.multiply(res, c)
 
-plt.subplot(2,3,2)
-plt.imshow(im1)
+    # prevod na celociselne matice
+    res = res.astype('uint8')
 
-plt.subplot(2,3,3)
-plt.imshow(res)
+    #plot
 
+    return (et1,im1,res)
 
-plt.subplot(2,3,4)
-plt.imshow(et1)
+if __name__ == "__main__":
+    jas1 = jasova_korekce('Cv04_porucha1.bmp','Cv04_porucha1_etalon.bmp')
+    jas2 = jasova_korekce('Cv04_porucha2.bmp','Cv04_porucha2_etalon.bmp')
 
-plt.subplot(2,3,5)
-plt.imshow(im1)
+    plt.figure()
+    plt.subplot(2,3,1)
+    plt.imshow(jas1[0])
 
-plt.subplot(2,3,6)
-plt.imshow(res)
+    plt.subplot(2,3,2)
+    plt.imshow(jas1[1])
 
-plt.show()
+    plt.subplot(2,3,3)
+    plt.imshow(jas1[2])
 
+    plt.subplot(2,3,4)
+    plt.imshow(jas2[0])
+
+    plt.subplot(2,3,5)
+    plt.imshow(jas2[1])
+
+    plt.subplot(2,3,6)
+    plt.imshow(jas2[2])
+
+    plt.show()
