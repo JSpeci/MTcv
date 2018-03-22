d70176dd58f168db4d1e52e6024904cdf0991292
Jan_Specian <jan.specian@tul.cz>
2018-03-22 10:17:54 +0100

    nacteni bin

--- cv05/cv05.py
+++ cv05/cv05.py
@@ -1,8 +1,26 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import struct
 
 #poslat na email LWZ
 
+
+def read_bytes_to_list(path):
+    vstup = []
+    index = 0
+    with open(path, "rb") as f:
+
+        byte = f.read(1)
+        while byte != '':
+            index = index + 1
+            vstup.append(struct.unpack('b', byte )[0])
+            byte = f.read(1)
+
+            if not byte:
+                break
+    return vstup
+
 if __name__ == "__main__":
-    pass
+    print(read_bytes_to_list("Cv05_LZW_data.bin"))
+    
