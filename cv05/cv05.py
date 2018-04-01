b0a5a78d140ba12a5e39a3b8bc9a856c510a84ca
Jan_Specian <jan.specian@tul.cz>
2018-04-01 17:42:43 +0200

    Finished

--- cv05/cv05.py
+++ cv05/cv05.py
@@ -1,12 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import struct
-import collections
+"""
+poslat na email LWZ
 
-#poslat na email LWZ
+"""
+import struct
+import pprint
 
 
 def read_bytes_to_list(path):
+    """
+    Nacte bajty ze vstupniho binarniho souboru a preve je na uint8
+    a vrati jako list
+    """
     vstup = []
     index = 0
     with open(path, "rb") as f:
@@ -14,71 +18,203 @@ def read_bytes_to_list(path):
         byte = f.read(1)
         while byte != '':
             index = index + 1
-            vstup.append(struct.unpack('b', byte )[0])
+            vstup.append(struct.unpack('b', byte)[0])
             byte = f.read(1)
 
             if not byte:
                 break
     return vstup
 
-def doLZW(dict_of_abc, list_of_data):
-
-    #zdroj https://www.youtube.com/watch?v=rTYOQbIeUNs
-
+def do_LZW_Compression(dict_of_abc, list_of_data):
+    """
+    LZW komprese
+    
+    dict_of_abc je vstupni slovnik dat
+        na kazdem indexu slovniku je list
+        v prubehu komprese se do nej pridavaji polozky
+    
+    list_of_data je posloupnost cisel ke kompresi
+    """
+    
+    # rozdil mezi None a [] je v pouziti metody extend na listu
+    
     result = []
+    P = []
+    C = [] # C je vzdy jeden prvek ze vstupu
+    PC = []
     
-    data = np.array(list_of_data)
-    dic = np.array(list(dict_of_abc.values()))
+    #how it works video xplanation https://www.youtube.com/watch?v=MQ4ObKv2L_M
     
-   
-    P = None
-    PC = None
-    for i in range(0,len(data)):
-        C = data[i]
-        if i==0:
-            PC = np.array([C])
+    for i in range(len(list_of_data)):
+        """
+        Cyklus pres vsecky vstupni prvky
+        """
+
+        C = []
+        C.append(list_of_data[i])
+
+        #PC je vzdy kombinace P a C
+        PC = []
+        PC.extend(P)
+        PC.extend(C)
+
+        index_founded = dict_cointains_list(dict_of_abc, PC)
+        if index_founded == -1:
+            #pokud PC neni ve slovniku, pridam ho tam a P = C
+            dict_of_abc[len(dict_of_abc) +1] = PC
+            #output P key in dictionary
+            result.append(dict_cointains_list(dict_of_abc, P))
+            P = C
         else:
-            PC = np.append(P,C)
+            #pokud PC je ve slovniku P = PC pro dalsi iteraci
+            P = PC
+    #pridani posledniho prvku
+    result.append(dict_cointains_list(dict_of_abc, P))
+    return dict_of_abc, result
+
+def do_LZW_DeCompression(dict_of_abc, list_of_data):
+    """
+    LZW Dekomprese
+    
+    dict_of_abc je vstupni slovnik dat
+        na kazdem indexu slovniku je list
+        v prubehu komprese se do nej pridavaji polozky
+    
+    list_of_data je posloupnost cisel pro dekompresi
+    """
+    
+    #https://www.youtube.com/watch?v=MQM_DsX-LBI
+    
+    out = []
+    predchozi_out = []
+    for i in range(len(list_of_data)):
+        new = []
+        new.extend(predchozi_out)
+        if list_of_data[i] in dict_of_abc:
+            o = dict_of_abc[list_of_data[i]]
+            out.extend(o)
+            predchozi_out = o
             
-        
-        #pokud existuje PC ve slovniku
-        if np.sum(np.isin(dic, PC)) > 0:
-            #P neposlat na vystup
-            #PC presunout do P
-            P = np.copy(PC)
-        else:
-            P = np.copy(C)
-            print("append")
-            dic = np.append(dic,PC)
-            #PC = np.array([])
-        #pokud neexistuje PC ve slovniku
-            #C presunout do P
-            #PC pridat do slovniku
-    
-        print(PC)
+            #pokud je o list, beru z nej pouze prvni prvek
+            if len(o) > 1:
+                new.append(o[0])
+            else:
+                new.extend(o)
 
-        
-    
+            index_founded = dict_cointains_list(dict_of_abc, new)
+            if index_founded == -1:
+                #pokud new neni ve slovniku, pridam ho tam
+                dict_of_abc[len(dict_of_abc) +1] = new
 
-    return result
+    return dict_of_abc, out
+
+def dict_cointains_list(dict_of_abc, item_list):
+    """
+    dict_of_abc vypada napriklad takto
+    3: [3],
+    4: [4],
+    5: [5],
+    6: [5, 4],
+    9: [4, 3],
+    8: [3, 5],
+    9: [5, 4, 1],
+    
+    item_list vypada napriklad takto [5,4,1]
+    
+    Metoda vraci, zda ve vstupnim slovniku je nejaka polozka typu list
+    a orvky toho listu jsou shodne s druhym vstupnim parametrem
+    
+    """
+
+    values = list(dict_of_abc.values())
+
+    #projdu vsecky listy ve slovniku
+    for i in range(len(values)):
+        #predpokladam ze ve slovniku je
+        finded = True
+        
+        for j in range(len(values[i])):
+            if len(item_list) == len(values[i]):
+                # kontrola po jednotlivych hodnotach
+                # logicky soucin - pokud jednou False, navzdy False
+                finded = finded and item_list[j] == values[i][j]
+            else:
+                finded = False
+
+        if finded:
+            # cyklus indexuje od 0, slovnik ale indexujeme-klicujeme od 1
+            return i + 1 
+
+    return -1
+
+def vypis_komprese(coded, vstupni_slovnik, vstupni_data):
+    """
+    Formatovany vystup komprese
+    """
+    # test kompresniho algoritmu
+    print("Test komprese")
+    print("  Vstupni data")
+    print(vstupni_data)
+    
+    print("\n Vstupni slovnik")
+    pprint.pprint(vstupni_slovnik)
+
+    dic = coded[0]
+    res = coded[1]
+    vypis = []
+    for i in range(len(res)):
+        vypis.extend(dic[res[i]])
+
+    print("\n  Vysledek")
+    print(res)
+
+    print("\n  Kompresni poměr")
+    komp_pomer = round(len(res)/len(data) * 100)
+    print(komp_pomer, " % z puvodni delky dat")
+
+    print("\n  Slovnik")
+    pprint.pprint(dic)
+
+    print("\n  Zpetne sestaveni vstup a sestaveni ")
+    print(data)
+    print(vypis)
+
+def vypis_dekomprese(decoded, vstupni_slovnik, vstupni_data):
+    """
+    Komentar
+    """
+    # test dekompresniho algoritmu
+    print("\n  Test dekomprese")
+    print("  Vstupni data")
+    print(vstupni_data)
+
+    print("\n Vstupni slovnik")
+    pprint.pprint(vstupni_slovnik)
+
+    dic = decoded[0]
+    res = decoded[1]
+    vypis = []
+    for i in range(len(res)):
+        vypis.extend(dic[res[i]])
+
+    print("\n  dekomprimovany Vysledek")
+    print(res)
+
+    print("\n  Slovnik vznikly pri dekompresi")
+    pprint.pprint(dic)
 
 if __name__ == "__main__":
+
     data = read_bytes_to_list("Cv05_LZW_data.bin")
-    abc = {1:1,2:2,3:3,4:4,5:5}
 
-    data2 = ['w','a','b','b','a','w','a','b','b','a']
+    abc = {1:[1], 2:[2], 3:[3], 4:[4], 5:[5]}
+    vstupni_slovnik = abc.copy()
+    coded = do_LZW_Compression(abc, data)
+    vypis_komprese(coded, vstupni_slovnik, data)
 
-    abc2 = {
-        1:['a'],
-        2:['b'],
-        3:['w']
-    }
-    
-    coded = doLZW(abc,data)
-    
-    a = np.array([1,2,3,4,5])
-    b = np.isin([7,8],a)
-    print(np.sum(b))
+    abc = {1:[1], 2:[2], 3:[3], 4:[4], 5:[5]}
+    vstupni_slovnik = abc.copy()
+    vstupni_data = coded[1].copy()
+    decoded = do_LZW_DeCompression(abc, coded[1])
 
-
-    
+    vypis_dekomprese(decoded, vstupni_slovnik, vstupni_data)
